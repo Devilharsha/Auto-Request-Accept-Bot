61294e20b994fac10d1554b83d149a965a32dfe6
Mx Nitro <95213910+Devilharsha@users.noreply.github.com>
2023-10-19 11:29:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,38 @@
 import os
 import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name)
 
-pr0fess0r_99 = Client(
-    "Bot Started Please Subscribe OpusTechz",
-    bot_token=os.environ["BOT_TOKEN"],
-    api_id=int(os.environ["API_ID"]),
-    api_hash=os.environ["API_HASH"]
-)
+# Initialize the Pyrogram Client
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # Define the chat or channel ID to monitor
-CHAT_ID = os.environ.get("CHAT_ID")
-TEXT = os.environ.get("BAN_WELCOME_MESSAGE", "Hello {mention}\nYou've been banned from {title}.")
-LEFTED = os.environ.get("BAN_WELCOME", "on").lower()
+chat_id = os.getenv("CHAT_ID")
 
-@pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
-async def start(client, message):
-    approvedbot = await client.get_me()
-    button = [
-        [
-            InlineKeyboardButton("Subscribe to OpusTechz", url="https://t.me/OpusTechz")
-        ]
-    ]
-    await message.reply_text(text="**Hello! I am a simple bot. For your chat's protection, I'll ban anyone who leaves.**", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+# Start command handler
+@app.on_message(filters.command("start") & filters.chat(chat_id))
+async def start_command(client, message):
+    await message.reply_text("Welcome to the Channel Monitor Bot! I will automatically ban members who leave the channel.")
 
-@pr0fess0r_99.on_chat_member_left()
-async def ban_left_member(client, message):
+# Member left handler
+@app.on_message(filters.chat(chat_id) & filters.left_chat_member)
+async def on_member_left(client, message):
+    member = message.left_chat_member
     try:
-        user = message.left_chat_member
-        chat = message.chat
-        if user:
-            logger.info(f"{user.first_name} left the chat ⚡")
-            await client.kick_chat_member(chat.id, user.id)
-            logger.info(f"{user.first_name} has been banned ⚡")
-            if LEFTED == "on":
-                await client.send_message(chat.id, TEXT.format(mention=user.mention, title=chat.title))
+        if member:
+            user = member.user
+            logger.info(f"Banning user: {user.first_name} (ID: {user.id})")
+            await client.kick_chat_member(chat_id, user.id)
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    logger.info("Bot Started, Monitoring for Members Leaving and Sending Ban Messages")
-    pr0fess0r_99.run()
+    logger.info("Bot Started, Monitoring for Members Leaving and Banning Them")
+    app.run()
